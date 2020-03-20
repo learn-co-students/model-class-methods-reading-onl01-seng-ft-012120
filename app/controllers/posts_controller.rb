2b83ff001522167258b974c260189b6c377a1f53
reddevilcero <reddevil_cero@hotmail.com>
2020-03-20 17:45:59 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,23 @@
 class PostsController < ApplicationController
-
+  helper_method :params
   def index
-    @posts = Post.all
+        # provide a list of authors to the view for the filter control
+    @authors = Author.all
+  
+    # filter the @posts list based on user input
+    if !params[:author].blank?
+      @posts = Post.by_author(params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.from_today
+      else
+        @posts = Post.old_news
+      end
+    else
+      # if no filters are applied, show all posts
+      @posts = Post.all
+    end
+
   end
 
   def show
@@ -13,7 +29,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params)
+    @post = Post.new(post_params)
     @post.save
     redirect_to post_path(@post)
   end
@@ -26,5 +42,10 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :description)
   end
 end
